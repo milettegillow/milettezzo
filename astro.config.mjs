351fca79b82f81b935ca9d8b31b6483ce4f5bdb3
Milette Gillow <miletteriis@live.com>
2026-01-07 08:43:57 +0000

    another test to stop this 405 madness

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
-export default defineConfig({
+export default {
+  output: "server",
   adapter: vercel(),
-});
+};
